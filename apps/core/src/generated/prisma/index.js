50efb346114ee00681fffd71efa9a3d7cadb0eaf
matiascarinidev <matiascarinidev@gmail.com>
2026-07-06 23:58:36 -0300

    Fix: regenerar clientes Prisma correctos

--- apps/core/src/generated/prisma/index.js
+++ apps/core/src/generated/prisma/index.js
@@ -372,7 +372,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
